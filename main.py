ae4b08396f7ac00d918ff430381d2d9d89a6829a
Gustavo Costa <gustavomiamispaces@gmail.com>
2023-12-19 12:19:33 -0500

    create_all in main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from foreclosure_suite.database.seed import DataSeed
+from foreclosure_suite.database import models
+from foreclosure_suite.database.config import engine
 
 def main():
     
+    models.Model.metadata.create_all(bind = engine)
     seeder = DataSeed()
     seeder.seed_data()
 
