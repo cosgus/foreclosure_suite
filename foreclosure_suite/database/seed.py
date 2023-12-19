585ee1f6ae6f0cd0323821dddf90eb90500d3150
Gustavo Costa <gustavomiamispaces@gmail.com>
2023-12-19 17:29:26 -0500

    added logger to seed
    Removed all prints and added logger to seed.py

--- foreclosure_suite/database/seed.py
+++ foreclosure_suite/database/seed.py
@@ -5,6 +5,7 @@ from sqlalchemy import func
 from foreclosure_suite.database import models
 from foreclosure_suite.database.config import session, engine
 from foreclosure_suite import scrapers
+from foreclosure_suite.logger import get_logger
 from utils import daterange
 
 BATCH_SIZE = 100
@@ -21,16 +22,18 @@ class DataSeed:
         self.appraiser_scraper = scrapers.appraiser.AppraiserScraper()
         self.court_scraper = scrapers.courts.CourtScraper()
 
+        self.logger = get_logger(__name__)
+
     def seed_data(self):
 
         batched = 0
         
         for single_date in daterange(self.start_date, datetime.now()):
             aids_list = self.foreclosure_scraper.get_days_aids(single_date)
-            print(single_date)
-            for idx, aid in enumerate(aids_list):
+            self.logger.info(single_date)
+            for aid in aids_list:
 
-                print(f'     {idx} - {aid}')
+                self.logger.info(f'     {batched+1} - {aid}')
                 aid_data = self.handle_auction(aid)
                 auction_data = self.foreclosure_scraper.parser.extract_auction_property_data(aid_data['html'])
 
@@ -38,9 +41,11 @@ class DataSeed:
                 case_number = auction_data['case_number']
                 
                 if parcel_id:
+                    self.logger.info('         ' + parcel_id)
                     self.handle_appraiser(parcel_id)
 
                 if case_number:
+                    self.logger.info('         ' + case_number)
                     self.handle_court(case_number)
 
                 batched += 1
@@ -91,13 +96,11 @@ class DataSeed:
         return aid_data
     
     def handle_appraiser(self, parcel_id):
-        print('         ' + parcel_id)
         if not self.exists_in_table(parcel_id, models.AppraiserLake):
             appraiser_data = self.create_appraiser_data(parcel_id)
             self.session.add(models.AppraiserLake(**appraiser_data))
             
     def handle_court(self, case_number):
-        print('         ' + case_number)
         if not self.exists_in_table(case_number, models.CourtLake):
             court_data = self.create_court_data(case_number)
             self.session.add(models.CourtLake(**court_data))
