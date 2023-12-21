bb07298c914458d17d86898d4e9be5a32adb2e27
Gustavo Costa <gustavomiamispaces@gmail.com>
2023-12-20 20:22:51 -0500

    checking if exists
    Added a ModelMixin class to models with a class method for checking if
    entry exists. I see the redundancy of this solution but it is nice and
    readable so I kept it. There is also a get or create class method but it
    turned out to not serve my purpose as I want to check if exists before
    I scrape the data. Other changes to allow for this

--- foreclosure_suite/database/models.py
+++ foreclosure_suite/database/models.py
@@ -5,9 +5,12 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, DateTime, Integer, JSON, String, Boolean, BigInteger
 
 from foreclosure_suite.database.config import session, engine
+from foreclosure_suite.logger import get_logger
 
 Model = declarative_base()
 Model.query = session.query_property()
+
+logger = get_logger(__name__)
 
 class Table(enum.Enum):
 
@@ -16,14 +19,28 @@ class Table(enum.Enum):
     COURT_RAW_DATA      = 'court_raw_data'
     APPRAISER_RAW_DATA  = 'appraiser_raw_data'
     
-
-class TimeStampedModel(Model):
+class ModelMixin(Model):
     __abstract__ = True
 
     created_at = Column(DateTime, default=datetime.utcnow())
     updated_at = Column(DateTime, onupdate=datetime.utcnow())
 
-class DataLake(TimeStampedModel):
+    @classmethod
+    def get_or_create(cls, session, **kwargs):
+        entry = session.query(cls).filter_by(**kwargs).first()
+        if not entry:
+            logger.info('Inserting')
+            entry = cls(**kwargs)
+            session.add(entry)
+        return entry
+
+    @classmethod
+    def exists_in_table(cls, session, **kwargs):
+        if session.query(cls).filter_by(**kwargs).first():
+            logger.debug(f'{cls} - Already Inserted')
+            return True
+        
+class DataLake(ModelMixin):
     __abstract__ = True
 
     parsed = Column(Boolean, default=False)
@@ -58,11 +75,11 @@ class CourtLake(DataLake):
     __tablename__ = "court_raw_data"
 
     id = Column(Integer, primary_key = True, autoincrement = True)
-    case_number = Column(String)
+    case_number = Column(String, unique = True)
     html = Column(String)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}, AID: {self.id}'
+        return f'{self.__class__.__name__}, Case Number: {self.case_number}'
 
 if __name__ == "__main__":
     Model.metadata.create_all(engine)
--- foreclosure_suite/database/seed.py
+++ foreclosure_suite/database/seed.py
@@ -1,21 +1,22 @@
 from datetime import datetime
+import json
 
 from sqlalchemy import func
 
-from foreclosure_suite.database import models
+from foreclosure_suite.database.models import AppraiserLake, AuctionLake, CourtLake, Scraped, Table, Model
 from foreclosure_suite.database.config import session, engine
 from foreclosure_suite import scrapers
 from foreclosure_suite.logger import get_logger
-from utils import daterange
+from utils import daterange, convert_folio_to_int
 
-BATCH_SIZE = 100
+BATCH_SIZE = 1
 
 class DataSeed:
 
     def __init__(self, alchemy_session = session):
 
         self.session = alchemy_session
-        self.tables = models.Table
+        self.tables = Table
         self.start_date = self.get_start_date()
 
         self.foreclosure_scraper = scrapers.foreclosure.ForeclosureScraper()
@@ -35,82 +36,89 @@ class DataSeed:
 
                 self.logger.info(f'     {batched+1} - {aid}')
                 aid_data = self.handle_auction(aid)
-                auction_data = self.foreclosure_scraper.parser.extract_auction_property_data(aid_data['html'])
+                auction_data = self.foreclosure_scraper.parser.extract_auction_property_data(aid_data.html)
 
                 parcel_id = auction_data['parcel_id']
                 case_number = auction_data['case_number']
                 
                 if parcel_id:
-                    self.logger.info('         ' + parcel_id)
+                    self.logger.info(f'         {parcel_id}')
                     self.handle_appraiser(parcel_id)
 
                 if case_number:
-                    self.logger.info('         ' + case_number)
+                    self.logger.info(f'         {case_number}')
                     self.handle_court(case_number)
 
                 batched += 1
                 if batched % BATCH_SIZE == 0:
                     self.session.commit()
-            self.session.add(models.Scraped(date = single_date))
+            self.session.add(Scraped(date = single_date))
         self.session.commit()
 
-    def insert(self, data: models.Model):
+    def insert(self, data: Model):
         if not self.check_if_exists(data):
             self.session.add(data)
 
     
     def get_start_date(self) -> datetime:
 
-        start_date = self.session.query(func.max(models.Scraped.date)).scalar()
+        start_date = self.session.query(func.max(Scraped.date)).scalar()
         if not start_date: 
-            start_date = datetime(2010,1,1)
+            start_date = datetime(2010,1,11)
         return start_date
 
     def create_aid_data(self, aid):
         aid_data = {
-            'id': aid,
-            'xhr': self.foreclosure_scraper.get_aid_xhr_response(aid).json(),
+            'id': int(aid),
+            'xhr': json.dumps(self.foreclosure_scraper.get_aid_xhr_response(aid).json()),
             'html': self.foreclosure_scraper.get_aid_url_response(aid).text
         }
         return aid_data
     
     def create_appraiser_data(self, parcel_id):
-        id = int(parcel_id.replace('-',''))
         self.appraiser_scraper.set_parcel_id(parcel_id)
         appraiser_data = {
-            'id': id,
-            'json': self.appraiser_scraper.get_appraisers_json()
+            'id': convert_folio_to_int(parcel_id),
+            'json': json.dumps(self.appraiser_scraper.get_appraisers_json())
         }
         return appraiser_data
     
     def create_court_data(self, case_number):
         return {
             'case_number': case_number,
-            'html': self.court_scraper.post_court(case_number=case_number, timeout = 30)
+            'html': self.court_scraper.post_court(case_number=case_number, timeout = 30).text
         }
     
     def handle_auction(self, aid):
-        if not self.exists_in_table(aid, models.AuctionLake):
-            aid_data = self.create_aid_data(aid)
-            self.session.add(models.AuctionLake(**aid_data))
-        return aid_data
-    
+        aid_data = self.create_aid_data(aid)
+        entry = AuctionLake(**aid_data)
+        if not AuctionLake.exists_in_table(self.session, id = aid):
+            self.session.add(entry)
+        return entry
+        
+
     def handle_appraiser(self, parcel_id):
-        if not self.exists_in_table(parcel_id, models.AppraiserLake):
+        context_id = convert_folio_to_int(parcel_id)
+        if not AppraiserLake.exists_in_table(self.session, id = context_id):
             appraiser_data = self.create_appraiser_data(parcel_id)
-            self.session.add(models.AppraiserLake(**appraiser_data))
-            
+            entry = AppraiserLake(**appraiser_data)
+            self.session.add(entry)
+
     def handle_court(self, case_number):
-        if not self.exists_in_table(case_number, models.CourtLake):
+        if not CourtLake.exists_in_table(self.session, case_number = case_number):
             court_data = self.create_court_data(case_number)
-            self.session.add(models.CourtLake(**court_data))
+            entry = CourtLake(**court_data)
+            self.session.add(entry)
 
-    def exists_in_table(self, id: int, table: models.Model):
-        return False
-    
+def drop_all_tables():
+    AppraiserLake.__table__.drop(bind = engine)
+    CourtLake.__table__.drop(bind = engine)
+    AuctionLake.__table__.drop(bind = engine)
+    Scraped.__table__.drop(bind = engine)
+
 if __name__ == '__main__':
-
-    models.Model.metadata.create_all(bind=engine)
+    drop_all_tables()
+    Model.metadata.create_all(bind=engine)
     seeder = DataSeed(session)
     seeder.seed_data()
     
