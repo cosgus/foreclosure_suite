da2b26237668ce3d535129b97212c00b0f5df7c6
Gustavo Costa <gustavomiamispaces@gmail.com>
2023-12-25 14:02:59 -0500

    persistent seeder
    Forgot I was running seeder from main.py so I have added the while True
    and try/except to the main.py file to make the seeder persist through
    connection errors

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from requests.exceptions import ConnectionError
+from time import sleep
+
 from foreclosure_suite.database.seed import DataSeed
 from foreclosure_suite.database import models
 from foreclosure_suite.database.config import engine
+from foreclosure_suite.utils import get_logger
 
 def main():
     
+    logger = get_logger
     models.Model.metadata.create_all(bind = engine)
     seeder = DataSeed()
-    seeder.seed_data()
+    while True:
+        try:
+            seeder.seed_data()
+        except ConnectionError as e:
+            logger.warning(f'Connection Error - Restarting seeder\n {e}')
+            sleep(30)
 
 if __name__ == '__main__':
     main()
